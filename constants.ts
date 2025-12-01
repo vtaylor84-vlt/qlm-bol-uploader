9875c1f5b2ec049d0c6b1e907cdf36a485ce1e16
unknown <vtaylor84@gmail.com>
2025-12-01 00:07:06 -0600

    SYNTAX FIX: Removed unexpected closing bracket (]) in constants.ts that was causing build failure.

--- constants.ts
+++ constants.ts
@@ -46,7 +46,5 @@ export const THEME_CONFIG: Record<CompanyName, CompanyTheme> = {
 };
 
 export const STATES_US = [
-    // ... states array remains the same
-];
     'AL', 'AK', 'AZ', 'AR', 'CA', 'CO', 'CT', 'DE', 'FL', 'GA', 'HI', 'ID', 'IL', 'IN', 'IA', 'KS', 'KY', 'LA', 'ME', 'MD', 'MA', 'MI', 'MN', 'MS', 'MO', 'MT', 'NE', 'NV', 'NH', 'NJ', 'NM', 'NY', 'NC', 'ND', 'OH', 'OK', 'OR', 'PA', 'RI', 'SC', 'SD', 'TN', 'TX', 'UT', 'VT', 'VA', 'WA', 'WV', 'WI', 'WY'
 ];
