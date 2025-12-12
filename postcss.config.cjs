4de1a1f742dbda495cd804be3316b1916eea98e5
unknown <vtaylor84@gmail.com>
2025-12-11 20:39:59 -0600

    fix: FINAL Deployment CSS FIX - Deleted external PostCSS configs and added tailwind version check to build command.

--- postcss.config.cjs
+++
@@ -1,8 +0,0 @@
-// postcss.config.cjs (The absolute final, required configuration for Netlify)
-module.exports = {
-  plugins: [
-    // This explicitly names the package that PostCSS now uses, satisfying the error.
-    require('@tailwindcss/postcss'), 
-    require('autoprefixer'),
-  ],
-};
